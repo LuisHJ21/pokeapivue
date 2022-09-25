c01e192a83cc7db4f6fad9d5486b714556d3cf35
LuisHJ21 <joslu147@gmail.com>
2022-09-25 11:00:15 -0500

    poke api

---
+++ src/router/index.js
@@ -0,0 +1,38 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  //linkActiveClass:'active',
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/HomeView.vue')
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path:'/pokemon',
+      name:'pokemon',
+      component: () => import('../views/PokemonView.vue')
+    },
+    {
+      path:'/pokemon/:name',
+      name:'poke',
+      component: () => import('../views/PokeView.vue')
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "NotFound",
+      component: () => import("../views/NotFoundView.vue"),
+    }
+  ]
+})
+
+export default router
---
+++ src/views/PokemonView.vue
@@ -0,0 +1,31 @@
+<script setup>
+    import axios from 'axios'
+    import {ref} from 'vue'
+    import {RouterLink} from 'vue-router'
+
+    const pokemons = ref([])
+
+    const getData= async ()=>
+    {
+        try {
+            const {data}=await axios.get('https://pokeapi.co/api/v2/pokemon/')
+            //console.log(data.results)
+            pokemons.value=data.results
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    getData()
+
+</script>
+
+<template>
+    <h1>Lista de Pokemon</h1>
+    <ul>
+        <li v-for="pok in pokemons">
+            <router-link :to="`/pokemon/${pok.name}`">{{pok.name}}</router-link>
+            
+        </li>
+    </ul>
+</template>
